d307c2512047a837f694c1b9a6def98eaebb3c32
Cumont Léa <lea.cumont.pro@gmail.com>
2023-03-30 22:34:43 +0200

    New function : You can like posts and we get every books in the database

--- db.sql
+++ db.sql
@@ -17,6 +17,13 @@ CREATE TABLE book
     FOREIGN KEY (user_id) REFERENCES user(rowid)
 );
 
+CREATE TABLE like_book(
+    book_id INT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(rowid),
+    FOREIGN KEY (book_id) REFERENCES book(rowid)
+);
+
 INSERT INTO user VALUES ("Jean", "Livre", "admin", "pbkdf2:sha256:260000$z2RnKLODzFUmDmyP$241dc532c6575508b346a19971fcd9503f43d5524a688356218c193421f1ee75");
 INSERT INTO book VALUES (1,"Alice au pays des merveilles", "Lewis Carroll", "Soleil", "Un matin, Alice décide de suivre un mystérieux Lapin Blanc jusque dans son terrier. Chemin faisant, elle fait des rencontres surprenantes : une Reine de Coeur qui veut couper la tête de tout le monde, un Chapelier fou avec lequel elle prend le thé, ou encore un Chat qui arbore un sourire jusqu'aux oreilles", 29.90);
 INSERT INTO book VALUES (1,"Salem", "Stephen King", "Le livre de Poche", "Le Maine, 1970. Ben Mears revient à Salem et s'installe à Marsten House, inhabitée depuis la mort tragique de ses propriétaires, vingt-cinq ans auparavant.", 9.90);
